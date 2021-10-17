77027b2ac26e953f993f33551829206a519b02a8
kyo705 <mission0705@naver.com>
2021-10-17 22:40:52 +0900

    second commit. add comment in java code

--- Main.java
+++ Main.java
@@ -4,8 +4,9 @@ public class Main {
 
 	public static void main(String[] args) throws CloneNotSupportedException {
 		
+		//person 객체 생성
 		Person person1 = new Person("Lee", new Age(940705,28));
-		
+		//person 객체의 copy 메소드를 통한 프로토타입 설계
 		Person person2 = person1.copy();
 		person2.setName("Kim");
 		person2.getAge().setBirthday(970325);
--- Person.java
+++ Person.java
@@ -28,8 +28,10 @@ public class Person implements Cloneable{
 		this.age = age;
 	}
 	
+	//프로토타입 설계 메소드
 	public Person copy() throws CloneNotSupportedException {
 		Person person = (Person) this.clone();
+		//person객체의 필드값 중 age가 reference type이기 때문에 깊은 복사가 안됌. 따라서 직접 깊은 복사가 되게 만들어줌
 		person.setAge(new Age(this.getAge().getBirthday(),this.getAge().getAgenum()));
 		return person;
 	}
