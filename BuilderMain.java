b1262c2e70c53a75441fd154a7d023e143a02545
kyo705 <89891704+kyo705@users.noreply.github.com>
2021-10-18 15:14:10 +0900

    Builder Pattern

---
+++ BuilderMain.java
@@ -0,0 +1,14 @@
+package Builder;
+
+public class BuilderMain {
+
+	public static void main(String[] args) {
+		
+		Computer computer = ComputerBuilder.start()
+				.withCpu("юнез i7").withRam("16G").withStorage("250G SSD")
+				.build();
+		
+		System.out.println(computer.toString());
+	}
+
+}
