252d67d9ef34d478441614986b18c5493cee31ec
kyo705 <89891704+kyo705@users.noreply.github.com>
2021-10-18 23:07:23 +0900

    Builder Pattern

--- Builder Pattern/BuilderMain.java
+++ Builder Pattern/BuilderMain.java
@@ -5,7 +5,7 @@ public class BuilderMain {
 	public static void main(String[] args) {
 		
 		Computer computer = ComputerBuilder.start()
-				.withCpu("¿Œ≈⁄ i7").withRam("16G").withStorage("250G SSD")
+				.withCpu("√Ä√é√Ö√ö i7").withRam("16G").withStorage("250G SSD")
 				.build();
 		
 		System.out.println(computer.toString());
