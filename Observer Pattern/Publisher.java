642dc9c5b288a6f8a2da8ff1a770539592465c38
kyo705 <89891704+kyo705@users.noreply.github.com>
2022-04-03 22:45:07 +0900

    Delete Observer Pattern directory

--- Observer Pattern/Publisher.java
+++
@@ -1,9 +0,0 @@
-package Observer;
-
-public interface Publisher {
-
-	public void addObserver(Observer obs);
-	public void deleteObserver(Observer obs);
-	public void notifyObserver();
-	public void updateContents(String str);
-}
